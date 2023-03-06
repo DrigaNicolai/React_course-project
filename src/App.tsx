49001ef3cb5535b035a62938024361458043d1f3
DrigaNicolai <just.whyme7@gmail.com>
2023-03-06 13:25:43 +0200

    Some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React, {useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
 
 function App() {
   const [value, setValue] = useState("Input text");
 
   return (
     <div className="App">
-      <Counter /> {/* function created component */}
-      <ClassCounter /> {/* class created component */}
+      {/*<h1>Function created component</h1>*/}
+      {/*<Counter />*/}
+      {/*<h1>Class created component</h1>*/}
+      {/*<ClassCounter />*/}
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Some description
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
