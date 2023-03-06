0d012bdb9289adcfa173c5e7edf81da18e72a52a
DrigaNicolai <just.whyme7@gmail.com>
2023-03-06 13:20:17 +0200

    Class component for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   const [value, setValue] = useState("Input text");
 
   return (
     <div className="App">
-      <Counter />
+      <Counter /> {/* function created component */}
+      <ClassCounter /> {/* class created component */}
     </div>
   );
 }
