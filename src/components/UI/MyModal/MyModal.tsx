e399c77a193455ec165977e5840024b9b10f8d2f
DrigaNicolai <just.whyme7@gmail.com>
2023-03-07 22:37:31 +0200

    MyModal component

---
+++ src/components/UI/MyModal/MyModal.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from "./MyModal.module.css";
+
+interface MyModalProps {
+  children: React.ReactNode,
+  visible: boolean,
+  setVisible: (visible: boolean) => void
+}
+
+const MyModal = ({children, visible, setVisible}: MyModalProps) => {
+  const rootClasses = [classes.myModal];
+
+  if (visible) {
+    rootClasses.push(classes.active);
+  }
+
+  return (
+    <div
+      className={rootClasses.join(' ')}
+      onClick={() => setVisible(false)}
+    >
+      <div
+        className={classes.myModalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default MyModal;
