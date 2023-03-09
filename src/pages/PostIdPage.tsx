703a088b290247e659179648f80a7138978ebd95
DrigaNicolai <just.whyme7@gmail.com>
2023-03-09 18:38:11 +0200

    Get dynamic data for posts

--- src/pages/PostIdPage.tsx
+++ src/pages/PostIdPage.tsx
@@ -7,24 +7,48 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
   const params = useParams();
   const [post, setPost] = useState({} as any);
+  const [comments, setComments] = useState([] as Array<any>);
   const [fetchPostById, isLoading, error] = useFetching(async (id: number) => {
     const response = await PostService.getByID(id);
 
     setPost(response.data);
   });
+  const [fetchComments, isComLoading, comError] = useFetching(async (id: number) => {
+    const response = await PostService.getCommentsByPostID(id);
+
+    setComments(response.data);
+  });
 
   useEffect(() => {
     fetchPostById(Number(params.id));
+    fetchComments(Number(params.id));
   }, []);
 
   return (
     <div>
       <h1>Post's page with id = {params.id}</h1>
-      {isLoading
+      { isLoading
         ?
           <Loader />
         :
           <div>{post.id}. {post.title}</div>
+      }
+      <h1>
+        Comments
+      </h1>
+      { isComLoading
+        ?
+          <Loader />
+        :
+          <div>
+            { comments.map(comm =>
+                <div key={comm.id} style={ { marginTop: 15 } }>
+                  <h5>{comm.email}</h5>
+                  <div>{comm.body}</div>
+                </div>
+              )
+            }
+          </div>
       }
     </div>
   );
