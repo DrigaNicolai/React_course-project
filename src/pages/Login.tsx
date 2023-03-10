5ec4af411aa766605e2ededdc9ae85fcb44cd8d4
DrigaNicolai <just.whyme7@gmail.com>
2023-03-10 11:59:29 +0200

    Login and logout functions

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
+import {AuthContext} from "../context";
 
 const Login = () => {
+  const {isAuth, setIsAuth} = useContext(AuthContext);
+
+  const login = (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsAuth(true);
+    localStorage.setItem("authCourseProject", "true");
+  }
+
   return (
     <div>
       <h1>Login page</h1>
-      <form>
+      <form onSubmit={login}>
         <MyInput type="text" placeholder="Login" />
         <MyInput type="password" placeholder="Password" />
         <MyButton>Sign In</MyButton>
