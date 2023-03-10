09eb612cff373d8cf0fdc037269cffdbe64b3589
DrigaNicolai <just.whyme7@gmail.com>
2023-03-10 12:00:06 +0200

    Add isLoading state while it is checking key

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {publicRoutes, privateRoutes} from "../router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  const isAuth = false;
+  const {isAuth, isLoading} = useContext(AuthContext);
+
+  if (isLoading) {
+    return (
+      <Loader />
+    )
+  }
 
   return (
     isAuth
